86a1c35cba9e33ced5c2508ba7d1544627edfed4
sakshi <sakshi@DESKTOP-MSGM031>
2024-07-03 23:04:12 +0530

    Day Two

---
+++ DayTwo/src/Day2/BreakDemo.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+    	for(int i=5;i<10;i++) {
+    		if(i == 6) {
+    			// break
+    			
+    			System.out.println(i);
+    			System.out.println("Hello Friends");
+    			break;
+    		}
+    	}
+    }
+}
---
+++ DayTwo/src/Day2/ForEachLoop.java
@@ -0,0 +1,23 @@
+package Day2;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+    	int a[] = {10,20,30,40,50};
+    	
+    	// for integer array 
+    	for(int i : a) {
+    		System.out.println(i);
+    	}
+    	
+    	char ch[] = {'j','a','v','a'};
+    	for(char c : ch) {
+    		System.out.println(c);
+    	}
+    	
+    	String s1[] = {"Java", "Programming","Learning"};
+    	
+    	for(String s : s1) {
+    		System.out.println(s+ " ");
+    	}
+    }
+}
